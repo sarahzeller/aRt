d5c5fcf18fa52b0872cfb5d9f8ebd1b72ef45534
szeller42 <s.zeller@posteo.net>
2023-12-23 11:21:11 +0100

    ♻️ save OSM data to output folder

--- utils.R
+++ utils.R
@@ -1,15 +1,18 @@
 load_and_save_osm <- function(value_name) {
-  if (!paste0(value_name, ".rds") %in% list.files(here())) {
+  if (!paste0(value_name, ".rds") %in% list.files(here("output"))) {
     # load
     osm_data <- opq(bbox = "Europe") |>
       add_osm_feature(key = "name", value = value_name) |>
       osmdata_sf()
     # save
-    saveRDS(osm_data, paste0(value_name, ".rds") |> here())
+    if (! "output" %in% list.files(here())) {
+      here("output") |> dir.create()
+    }
+    saveRDS(osm_data, paste0("output/", value_name, ".rds") |> here())
     # return
     return(osm_data)
   } else {
-    paste0(value_name, ".rds") |>
+    paste0("output/", value_name, ".rds") |>
       here() |>
       readRDS()
   }
